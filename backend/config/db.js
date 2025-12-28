5a53152635ce35ebcbe2b7c1ca3ed03f48b8c992
Prashnat Singh <prashant9818101426@gmail.com>
2025-12-28 16:25:42 +0530

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb+srv://prashant9818101426_db_user:VjuN1ciCjDiA6Mgq@cluster0.mrtz2j2.mongodb.net/",
+      "Relace with mongodb url",
     );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
